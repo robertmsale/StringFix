b980b5048e3244aef5014cf9ce4f9e23ba1f9895
Robert Sale <robertmsale@zoho.com>
2019-12-27 17:39:57 -0800

    Added a whole slew of functionality

--- README.md
+++ README.md
@@ -1,3 +1,102 @@
 # StringFix
 
-A description of this package.
+A zero-dependency pure Swift library which adds useful functionality to the String Protocol. By extending String Protocol, all of these methods become available on Strings as well as Substrings and other SubSequences so you can chain them together. Most of the functionality in this library returns a subsequence rather than a new string, which saves on memory usage.
+
+## Installation
+### Prerequisites
+This package requires Swift 5 and Xcode 11 (package is untested on earlier versions of Swift and Xcode)
+
+### Add To Project
+Add this package to your package.swift
+```swift
+.package(url: "https://github.com/" from: "1.0.0")
+```
+
+## Usage
+```swift
+import StringFix
+```
+
+## Subscripts
+Subscripting with Integers is now allowed
+```swift
+"Hello World!"[2...6] // "llo W"
+"Hello World!"[6...]  // "World!"
+"Hello World!"[...4]  // "Hello"
+"Hello World!"[0..<11]  // "Hello World"
+```
+Subscripts can be chained together 
+```swift
+"https://just1guy.org"[8...][..<8] // "just1guy"
+```
+
+## Methods
+### between (_ begin: String, _ end: String? = nil) -> SubSequence
+```swift
+"##foo##".between("##")                      // "foo"
+"<a>some link</a>".between("<a>", "</a>")    // "some link"
+"## ##foo## ##".between("##")                // " ##foo## "
+```
+### outerBetween (_ begin: String, _ end: String? = nil) -> SubSequence
+```swift
+"##foo##".outerBetween("##")                      // "##foo##"
+"<a>some link</a>".outerBetween("<a>", "</a>")    // "<a>some link</a>"
+"## ##foo## ##".outerBetween("##")                // "## ##foo## ##"
+```
+### camelize () -> String
+```swift
+"id number".camelize()      // "idNumber"
+"HelloWorld".camelize()     // "helloWorld"
+"text_size".camelize()      // "textSize"
+"first-name".camelize()     // "firstName"
+```
+### collapseWhitespace() -> String
+```swift
+"  So  much \t\n space!  "  // "So much space!"
+```
+### ensureLeft() -> String
+```swift
+"/etc/config".ensureLeft("/")   // "/etc/config"
+"etc/config".ensureLeft("/")    // "/etc/config"
+```
+### ensureRight() -> String
+```swift
+"https://just1guy.org/".ensureRight("/")    // "https://just1guy.org/"
+"https://just1guy.org".ensureRight("/")     // "https://just1guy.org/"
+```
+### isAlpha() -> Bool
+```swift
+"foobar".isAlpha()      // true
+"f00b4r".isAlpha()      // false
+"FooBar".isAlpha()      // true
+```
+### isAlphaNumeric() -> Bool
+```swift
+"FooBar5".isAlphaNumeric()      // true
+"123abc".isAlphaNumeric()       // true
+"-32".isAlphaNumeric()          // false
+```
+### isNumeric() -> Bool
+```swift
+"123".isNumeric()       // true
+"f00b4r".isNumeric()    // false
+```
+### isEmpty() -> Bool
+```swift
+"   ".isEmpty()         // true
+"\n\t".isEmpty()        // true
+"  foo  ".isEmpty()     // false
+```
+### trimLeft() -> SubSequence
+```swift
+"   \n\t Hello World!".trimLeft()       // "Hello World!"
+```
+### trimRight() -> SubSequence
+```swift
+"Hello World!   \n\t  ".trimLeft()      // "Hello World!"
+```
+### trim() -> SubSequence
+```swift
+" \t Hello World! \t  ".trim()          // "Hello World!"
+```
+
--- Sources/StringFix/StringFix.swift
+++ Sources/StringFix/StringFix.swift
@@ -1,11 +1,161 @@
+import Foundation
 
 public extension StringProtocol {
+    //  Allow using integers as subscripts
     subscript(_ offset: Int)                     -> Element     { self[index(startIndex, offsetBy: offset)] }
     subscript(_ range: Range<Int>)               -> SubSequence { prefix(range.lowerBound+range.count).suffix(range.count) }
     subscript(_ range: ClosedRange<Int>)         -> SubSequence { prefix(range.lowerBound+range.count).suffix(range.count) }
     subscript(_ range: PartialRangeThrough<Int>) -> SubSequence { prefix(range.upperBound.advanced(by: 1)) }
     subscript(_ range: PartialRangeUpTo<Int>)    -> SubSequence { prefix(range.upperBound) }
     subscript(_ range: PartialRangeFrom<Int>)    -> SubSequence { suffix(Swift.max(0, count-range.lowerBound)) }
+    
+    /// Returns a subsequence derived from two bookends, including the bookends themselves
+    ///
+    /// - parameter begin: The left bookend
+    /// - parameter endsWith: The right bookend (optional)
+    ///
+    /// - returns: The subsequence (String, Substring, etc) between two bookends, or returns the whole string intact if subsequence cannot be found.
+    func outerBetween( _ begin: String, _ end: String? = nil ) -> SubSequence {
+        var startI = self.startIndex
+        var endI = self.endIndex
+        for i in 0 ..< self.count {
+            if self[ index( startIndex, offsetBy: i ) ..< endIndex ].hasPrefix( begin ) {
+                startI = index( startIndex, offsetBy: i )
+                break
+            }
+        }
+        for i in 0 ..< self.count {
+            if self[ startIndex ..< index( endIndex, offsetBy: -i) ].hasSuffix(end ?? begin) {
+                endI = index( endIndex, offsetBy: -i )
+                break
+            }
+        }
+        
+        return self[startI..<endI]
+    }
+    
+    /// Returns a subsequence derived from two bookends, excluding the bookends themselves
+    ///
+    /// - parameter begin: The left bookend
+    /// - parameter endsWith: The right bookend
+    ///
+    /// - returns: The subsequence (String, Substring, etc) between two bookends, or returns the whole string intact if subsequence cannot be found
+    func between( _ begin: String, _ end: String? = nil ) -> SubSequence {
+        let outer = outerBetween( begin, end )
+        return outer[ outer.index( outer.startIndex, offsetBy: begin.count ) ..< outer.index ( outer.endIndex, offsetBy: -(end?.count ?? begin.count ) ) ]
+    }
+    
+    /// Converts a string to camelcase
+    func camelize() -> String {
+        var nonLetterIndexes: [String.Index] = []
+        
+        for ( i, v ) in self.enumerated() {
+            if ( !v.isLetter ) { nonLetterIndexes.append( self.index( startIndex, offsetBy: i ) ) }
+        }
+        
+        var stringBuilder = ""
+        if nonLetterIndexes.count > 0 {
+            for i in 0..<nonLetterIndexes.count {
+                if i == 0 && nonLetterIndexes[i] != startIndex {
+                    stringBuilder += "\(self[ startIndex ..< nonLetterIndexes[i] ])".lowercased() + "\(nonLetterIndexes.count > 1 ? self[ index( after: nonLetterIndexes[i]) ..< nonLetterIndexes[i + 1] ] : self[ index( after: nonLetterIndexes[i] ) ..< endIndex])".lowercased().capitalized
+                } else if i == nonLetterIndexes.count - 1 && nonLetterIndexes[i] != self.endIndex {
+                    stringBuilder += "\(self[ index( after: nonLetterIndexes[i] ) ..< endIndex ])".lowercased().capitalized
+                } else {
+                    stringBuilder += "\(self[ index( after: nonLetterIndexes[i] ) ..< nonLetterIndexes[i + 1] ])".lowercased().capitalized
+                }
+            }
+        } else {
+            stringBuilder += "\(self[ startIndex ..< (firstIndex(where: { ($0.isUppercase) && ($0 != self[startIndex]) }) ?? endIndex) ])".lowercased() + "\(self[ (firstIndex(where: { ($0.isUppercase) && ($0 != self[startIndex]) }) ?? endIndex ) ..< endIndex])"
+        }
+        
+        return stringBuilder
+    }
+    
+    /// Removes excess whitespace, including new lines, carriage returns, tab space, etc.
+    func collapseWhitespace() -> String {
+        var stringBuilder = ""
+        for (i,v) in self.enumerated() {
+            if v.isWhitespace {
+                if (i == self.count - 1) && (!v.isWhitespace) { stringBuilder += "\(v)" }
+                else if (i == self.count - 1) { break }
+                else if !self[self.index(self.startIndex, offsetBy: i+1)].isWhitespace { stringBuilder += "\(v)" }
+            } else { stringBuilder += "\(v)" }
+        }
+        return String(stringBuilder.trim())
+    }
+    
+    /// Ensures prefix exists and, if not, returns a prepended string
+    func ensureLeft(_ prefix: String) -> String {
+        if self[self.startIndex..<self.index(self.startIndex, offsetBy: prefix.count)].contains(prefix) { return "\(self)" }
+        else { return "\(prefix)\(self)" }
+    }
+    
+    /// Ensures suffix exists and, if not, returns an appended string
+    func ensureRight(_ suffix: String) -> String {
+        if self[self.index(self.endIndex, offsetBy: -suffix.count)..<self.endIndex].contains(suffix) { return "\(self)"}
+        else { return "\(self)\(suffix)"}
+    }
+    
+    /// Is this string Alphabetical
+    func isAlpha() -> Bool {
+        for (_, v) in self.enumerated() { if !v.isLetter { return false } }
+        return true
+    }
+    
+    /// Is this string AlphaNumeric
+    func isAlphaNumeric() -> Bool {
+        for (_, v) in self.enumerated() { if !v.isLetter && !v.isNumber { return false } }
+        return true
+    }
+    
+    /// Does this string only contain numbers and decimal point
+    func isNumeric() -> Bool {
+        for (_, v) in self.enumerated() { if (!v.isNumber) && (v != ".") { return false } }
+        return true
+    }
+    
+    /// Is this string not nil but also empty or contain only whitespace?
+    func isEmpty() -> Bool {
+        for (_, v) in self.enumerated() { if (!v.isWhitespace) && (v != " ") { return false } }
+        return true
+    }
+    
+    /// Remove whitespace from the left
+    func trimLeft() -> SubSequence { return self[ (firstIndex( where: { !$0.isWhitespace } ) ?? startIndex)..<endIndex ] }
+    
+    /// Remove whitespace from the right
+    func trimRight() -> SubSequence { return self[ startIndex...(lastIndex( where: { !$0.isWhitespace } ) ?? endIndex) ] }
+    
+    /// Remove whitepace from the left and right
+    func trim() -> SubSequence { self.trimLeft().trimRight() }
+    
+    /// Remove the shell from a snail
+    func slugify() -> String {
+        var stringBuilder: String = ""
+        
+        for ( i, v ) in self.enumerated() {
+            if i == 0 {
+                if self[index(startIndex, offsetBy: i)].isLetter { stringBuilder += "\(v)" }
+            } else {
+                if self[index(startIndex, offsetBy: i)].isLetter { stringBuilder += "\(v)" }
+                else {
+                    if (self[index(startIndex, offsetBy: i + 1)].isLetter) && (i < self.count - 1) { stringBuilder += "-"}
+                }
+            }
+        }
+        return stringBuilder.lowercased()
+    }
+    
+    /// Remove all punctuation
+    func stripPunctuation() -> String {
+        var stringBuilder: String = ""
+        
+        for ( i, v ) in self.enumerated() {
+            if (v.isLetter) || (v.isWhitespace) { stringBuilder += "\(v)" }
+        }
+        
+        return stringBuilder
+    }
 }
 
 public extension LosslessStringConvertible {
@@ -18,4 +168,3 @@ public extension BidirectionalCollection {
         return self[i]
     }
 }
-
